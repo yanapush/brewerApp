bd2609e16015ba8cf679e64bfac359f522beb765
yanapush <pushkarevyy@mail.ru>
2022-03-22 23:05:26 +0400

    added registration

--- src/main/java/com/yanapush/BrewerApp/security/SecurityConfiguration.java
+++ src/main/java/com/yanapush/BrewerApp/security/SecurityConfiguration.java
@@ -140,7 +140,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors();
         http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/*").permitAll().anyRequest().authenticated();
+                .authorizeRequests().antMatchers("/api/*", "/register").permitAll().anyRequest().authenticated();
         http.apply(new JwtTokenConfigurer(tokenProvider));
     }
 }
